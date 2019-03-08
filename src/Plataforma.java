a9255afe7356deeb8180dee4164cd8b07598d6da
jconchac <jconchac@alumnos.unex.es>
2019-03-08 23:56:14 +0100

    Implementación básica clase Plataforma

---
+++ src/Plataforma.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Implementa el patrón de diseño Singleton
+ * // TODO Documentar clase TorreControl
+ *
+ * @author Juan Pablo García Plaza Pérez
+ * @author José Ángel Concha Carrasco
+ */
+public class Plataforma {
+
+    private static Plataforma instancia = null;   // Instancia Singleton de la Plataforma
+
+    private Lock monitor;
+    private Condition esperaAzucar;             // Variable de espera para las grúas de azúcar
+    private Condition esperaHarina;             // Variable de espera para las grúas de Harina
+    private Condition esperaSal;                // Variable de espera para las grúas de Sal
+    private Condition esperaMercante;           // Varible de espera para el barco mercante
+
+    /**
+     * Constructor por defecto
+     */
+    private Plataforma() {
+
+        monitor = new ReentrantLock(true);
+        esperaAzucar = monitor.newCondition();
+        esperaHarina = monitor.newCondition();
+        esperaSal = monitor.newCondition();
+        esperaMercante = monitor.newCondition();
+
+    }
+    
+    /**
+     * @return Instancia Singleton de la Plataforma
+     */
+    public synchronized static Plataforma recuperarInstancia() {
+        if (instancia != null)
+            return instancia;
+        else
+            instancia = new Plataforma();
+
+        return instancia;
+    }
+
+
+}
